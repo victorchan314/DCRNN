c42b1c070af29f3206949ddc9fed76bc5589130c
Victor Chan <victorchan88888888@gmail.com>
2019-06-23 13:37:21 -0700

    Updated adjacency matrix generation to use phase times instead of road distances

--- scripts/gen_adj_mx.py
+++ scripts/gen_adj_mx.py
@@ -18,7 +18,7 @@ def get_adjacency_matrix(distance_df, sensor_ids, normalized_k=0.1):
     """
     num_sensors = len(sensor_ids)
     dist_mx = np.zeros((num_sensors, num_sensors), dtype=np.float32)
-    dist_mx[:] = np.inf
+    dist_mx[:] = 0
     # Builds sensor id to index map.
     sensor_id_to_ind = {}
     for i, sensor_id in enumerate(sensor_ids):
@@ -26,19 +26,23 @@ def get_adjacency_matrix(distance_df, sensor_ids, normalized_k=0.1):
 
     # Fills cells in the matrix with distances.
     for row in distance_df.values:
-        if row[0] not in sensor_id_to_ind or row[1] not in sensor_id_to_ind:
-            continue
-        dist_mx[sensor_id_to_ind[row[0]], sensor_id_to_ind[row[1]]] = row[2]
+        sensor_from = str(int(row[0]))
+        sensor_to = str(int(row[1]))
+        if sensor_from not in sensor_id_to_ind or sensor_to not in sensor_id_to_ind:
+            raise Exception("Error: edge sensors {} and {} not in sensors list".format(row[0], row[1]))
+        dist_mx[sensor_id_to_ind[sensor_from], sensor_id_to_ind[sensor_to]] = row[2]
 
     # Calculates the standard deviation as theta.
-    distances = dist_mx[~np.isinf(dist_mx)].flatten()
+    distances = dist_mx[dist_mx != 0].flatten()
     std = distances.std()
-    adj_mx = np.exp(-np.square(dist_mx / std))
+    #adj_mx = np.exp(-np.square(dist_mx / std))
+    adj_mx = dist_mx / std
+
     # Make the adjacent matrix symmetric by taking the max.
     # adj_mx = np.maximum.reduce([adj_mx, adj_mx.T])
 
     # Sets entries that lower than a threshold, i.e., k, to zero for sparsity.
-    adj_mx[adj_mx < normalized_k] = 0
+    # adj_mx[adj_mx < normalized_k] = 0
     return sensor_ids, sensor_id_to_ind, adj_mx
 
 
@@ -48,9 +52,9 @@ if __name__ == '__main__':
                         help='File containing sensor ids separated by comma.')
     parser.add_argument('--distances_filename', type=str, default='data/sensor_graph/distances_la_2012.csv',
                         help='CSV file containing sensor distances with three columns: [from, to, distance].')
-    parser.add_argument('--normalized_k', type=float, default=0.1,
-                        help='Entries that become lower than normalized_k after normalization are set to zero for sparsity.')
-    parser.add_argument('--output_pkl_filename', type=str, default='data/sensor_graph/adj_mat.pkl',
+#    parser.add_argument('--normalized_k', type=float, default=0.1,
+#                        help='Entries that become lower than normalized_k after normalization are set to zero for sparsity.')
+    parser.add_argument('--output_pkl_filename', type=str, default='data/sensor_graph/adj_mat_2.pkl',
                         help='Path of the output file.')
     args = parser.parse_args()
 
